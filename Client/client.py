68b2c24bdfce5cb4631bedcfb074141e904b1344
Marshall Royce <mrro253@uky.edu>
2022-04-21 21:48:33 -0400

    Got the file upload to remote folder working as well as gracefully returning to command prompt. I'll be getting the speed next.

--- Client/client.py
+++ Client/client.py
@@ -1,29 +1,44 @@
 import socket
 
 IP = "localhost"
-PORT = 4455
+PORT = 4282
 ADDR = (IP, PORT)
 FORMAT = "utf-8"
 SIZE = 1024
+SEPARATOR ="<SEPARATOR>"
 
 def main():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(ADDR)
+    while True:
+        userInput = input(">")
+        userInput = userInput.split(" ")
+        command = userInput[0]
+        print(userInput)
+        if (command == "CONNECT"):
+            IP = userInput[1]
+            PORT = userInput[2]
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect(ADDR)
+            client.sendall(f"{command}{SEPARATOR}{IP}{SEPARATOR}{PORT}".encode())
+            msg = client.recv(SIZE).decode(FORMAT)
+            print(f"[SERVER]: {msg}")
+        if (command == "UPLOAD"):
+            file = open(userInput[1], "r")
+            data = file.read()
 
-    file = open("data/test.txt", "r")
-    data = file.read()
+            client.sendall(f"{command}{SEPARATOR}{userInput[1]}".encode())
 
-    client.send("test.txt".encode(FORMAT))
+            msg = client.recv(SIZE).decode(FORMAT)
+            print(f"[SERVER]: {msg}")
 
-    msg = client.recv(SIZE).decode(FORMAT)
-    print(f"[SERVER]: {msg}")
-
-    client.send(data.encode(FORMAT))
-    msg = client.recv(SIZE).decode(FORMAT)
-    print(f"[SERVER]: {msg}")
-
-    file.close()
-    client.close()
+            client.send(data.encode(FORMAT))
+            msg = client.recv(SIZE).decode(FORMAT)
+            print(f"[SERVER]: {msg}")
+            file.close()
+        if command == "QUIT":
+            client.send(userInput.encode(FORMAT))
+            msg = client.recv(SIZE).decode(FORMAT)
+            print(f"[SERVER]: {msg}")
+            client.close()
 
 if __name__ == "__main__":
     main()
--- Server/server.py
+++ Server/server.py
@@ -1,34 +1,46 @@
 import socket
 
 IP = "localhost"
-PORT = 4455
+PORT = 4282
 ADDR = (IP, PORT)
 FORMAT = "utf-8"
 SIZE = 1024
+SEPARATOR = "<SEPARATOR>"
 
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(ADDR)
     server.listen()
     print("[Listening]")
-
+    conn, addr = server.accept()
+    print(f"[NEW CONNECTION] {addr} connected.")
     while True:
-        conn, addr = server.accept()
-        print(f"[NEW CONNECTION] {addr} connected.")
+        token = conn.recv(SIZE).decode(FORMAT)
+        token = token.split(SEPARATOR)
+        command = token[0]
+        print("[RECV] Command received.")
+        print("Command:", command)
+        if (command == "UPLOAD"):
+            print("upload")
+            filename = token[1]
+            file = open("server_data/"+filename, "w")
+            conn.send("Filename received".encode(FORMAT))
 
-        filename = conn.recv(SIZE).decode(FORMAT)
-        print("[RECV] Filename received.")
-        file = open(filename, "w")
-        conn.send("Filename received".encode(FORMAT))
+            data = conn.recv(SIZE).decode(FORMAT)
+            print(f"[RECV] File data received.")
+            file.write(data)
+            conn.send("File data received.".encode(FORMAT))
 
-        data = conn.recv(SIZE).decode(FORMAT)
-        print(f"[RECV] File data received.")
-        file.write(data)
-        conn.send("File data received.".encode(FORMAT))
-
-        file.close()
-        conn.close()
-        print(f"[DISCONNECTED] {addr} disconnected")
+            file.close()
+        elif (command == "CONNECT"):
+            print("Getting connect.")
+            conn.send("Connection established".encode(FORMAT))
+        elif command == "QUIT":
+            conn.send("Connection closed.")
+            conn.close()
+            print(f"[DISCONNECTED] {addr} disconnected")
+        else:
+            print("idk")
 
 if __name__ == "__main__":
     main()
